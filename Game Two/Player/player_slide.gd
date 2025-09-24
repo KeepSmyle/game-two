73ed956a9acbeb42a0eec26167016e2964e93eae
David Koch <koch.david95@web.de>
2025-09-25 00:13:47 +0200

    added slide functionalities

--- Game Two/Player/player_slide.gd
+++ Game Two/Player/player_slide.gd
@@ -1,13 +1,36 @@
 extends "res://Player/Player.gd"
 
+var MAX_SPEED = 800
+var ACCELERATION = 15
 
 func _ready():
 	pass
 
-
 func _physics_process(delta):
-	_standart_movement(delta)
 	
-	if collision && Input.is_action_pressed("peng_action"):
-		if direction > 0:
-			pass#velocity.x = -1 * SPEED * bounce_multi
+	if Input.is_action_pressed("peng_action"):
+		$AnimatedSprite2D.rotation_degrees = 90
+		_slide_movement(delta)
+	else:
+		$AnimatedSprite2D.rotation = 0
+		_standart_movement(delta)
+
+func _slide_movement(delta):
+	#todo: rotate sprite
+	
+	direction = Input.get_axis("peng_moveLeft", "peng_moveRight")
+	
+	if is_on_floor() && direction:
+		if direction == -1:
+			get_node("AnimatedSprite2D").flip_h = true
+		elif direction == 1:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION)
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, 5)
+	
+	# adds gravity 
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	
+	collision = move_and_slide()
--- Game Two/arena.gd
+++ Game Two/arena.gd
@@ -3,6 +3,7 @@ extends Node2D
 var Bounce_Player = load("res://Player/Player_bounce.tscn")
 var Hook_Player = load("res://Player/Player_hook.tscn")
 var Hop_Player = load("res://Player/Player_hop.tscn")
+var Slide_Player = load("res://Player/Player_slide.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +16,8 @@ func _ready():
 			playerScene = Bounce_Player.instantiate()
 		"Hop_Guy":
 			playerScene = Hop_Player.instantiate()
+		"Slide_Guy":
+			playerScene = Slide_Player.instantiate()
 			
 	find_child("Player").add_child(playerScene)
 	playerScene.set_owner(playerScene.get_parent())
