3cc0b82192cb15f6a0f32e83aa27ad80ef32544a
David Koch <koch.david95@web.de>
2025-10-22 13:19:52 +0200

    adjusted reset code in loop

--- Game Two/Level/loop.gd
+++ Game Two/Level/loop.gd
@@ -11,6 +11,8 @@ var sprites = [load("res://images/Characters/Frog.jpg"),
 				load("res://images/Characters/Ghost.jpg"),
 				load("res://images/Characters/Peng.jpg"),
 				load("res://images/Characters/Spider.jpg")]
+				
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -41,11 +43,24 @@ func _add_node_to_player(scene):
 	scene.set_owner(scene.get_parent())
 
 func _reset():
+	_reset_player()
+	_reset_objects()
+	
+func _reset_player():
 	$Plattforms/Plattform1/Plattform_Sprite_Start.set_texture(sprites[sprite_number])
 	rng.randf_range(1, 5)
 	player.global_position.x -= 1100
 	sprite_number = rng.randf_range(0, 3)
 	$Plattforms/Plattform2/Plattform_Sprite_End.set_texture(sprites[sprite_number])
+	
+func _reset_objects():
+	for node: Node in $Generated.get_children():
+		node.queue_free()
+	_spawn_new_objects()
+	
+#TODO: put generated objects in seperate scenes to spawn them
+func _spawn_new_objects():
+	pass 
 
 func _on_mobspawn_timer_timeout():
 	pass#_spawn()
